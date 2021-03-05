514715753b39f7cf29c0eafcbcd61f64ad845567
Keith <dreamihad@gmail.com>
2021-03-04 23:30:18 -0500

    and it is

--- midoLab.py
+++ midoLab.py
@@ -4,16 +4,22 @@ from rtmidi.midiutil import open_midioutput
 
 import mido
 
+import random
 
 mid = MidiFile('midi.mid')
 #port = PrintPort()
 midiout, port_name = open_midioutput(1)
+noteShift = random.randint(-2, 2)
 for message in mid.play():
     # print(msg)
     # port.send(msg)
     if(message.type == 'note_on'):
         print('send note ON ')
-        midiout.send_message([0x90, message.note, message.velocity])
+        midiout.send_message(
+            [0x90, message.note + noteShift, message.velocity])
     if(message.type == 'note_off'):
 
-        midiout.send_message([0x80, message.note, message.velocity])
+        midiout.send_message(
+            [0x80, message.note + noteShift, message.velocity])
+
+del midiout
