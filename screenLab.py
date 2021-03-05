1124717e0d3af3b128dab36c059ef2500bc532f2
Keith <dreamihad@gmail.com>
2021-03-05 16:36:00 -0500

    o

--- screenLab.py
+++ screenLab.py
@@ -126,9 +126,9 @@ while True:
     # Write two lines of text.
 
     draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+    #draw.text((x, top+8),     str(CPU), font=font, fill=255)
+    #draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
+    #draw.text((x, top+25),    str(Disk),  font=font, fill=255)
 
     # Display image.
     disp.image(image)
