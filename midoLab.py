2540fda82d5d491e7c8c0f1695b75898a27c5c92
Keith <dreamihad@gmail.com>
2021-03-04 23:21:03 -0500

    and it is

--- midoLab.py
+++ midoLab.py
@@ -5,25 +5,15 @@ from rtmidi.midiutil import open_midioutput
 import mido
 
 
-class PrintPort(BaseOutput):
-    def _send(self, message):
-        # print(message.dict())
-        if(message.type == 'note_on'):
-            print('send note ON ')
-            self.midiout.send_message([0x90, message.note, message.velocity])
-        if(message.type == 'note_off'):
-
-            self.midiout.send_message([0x80, message.note, message.velocity])
-
-    def __open(self, **kwargs):
-        print('Open port ')
-        midiout, port_name = open_midioutput(1)
-        self.midiout = midiout
-
-
 mid = MidiFile('midi.mid')
-port = PrintPort()
-
-for msg in mid.play():
+#port = PrintPort()
+midiout, port_name = open_midioutput(1)
+for message in mid.play():
     # print(msg)
-    port.send(msg)
+    # port.send(msg)
+    if(message.type == 'note_on'):
+        print('send note ON ')
+        midiout.send_message([0x90, message.note, message.velocity])
+    if(message.type == 'note_off'):
+
+        midiout.send_message([0x80, message.note, message.velocity])
