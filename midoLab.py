f7614a5b716e30d44e85bf6f9f6945aab4ffb207
Keith <dreamihad@gmail.com>
2021-03-04 23:11:47 -0500

    and it is

--- midoLab.py
+++ midoLab.py
@@ -1,11 +1,18 @@
 from mido.ports import BaseOutput
 from mido import MidiFile
+from rtmidi.midiutil import open_midioutput
+
 import mido
 
 
 class PrintPort(BaseOutput):
     def _send(self, message):
         print(message.dict())
+        self.midiout.send_message(message)
+
+    def __open(self, **kwargs):
+        midiout, port_name = open_midioutput(1)
+        self.midiout = midiout
 
 
 mid = MidiFile('midi.mid')
