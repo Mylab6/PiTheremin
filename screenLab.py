f97ad58f679619de9bb118b62348f8fcb7987bdd
Keith <dreamihad@gmail.com>
2021-03-05 16:42:38 -0500

    ok

--- screenLab.py
+++ screenLab.py
@@ -125,7 +125,7 @@ while True:
 
     # Write two lines of text.
 
-    draw.text((x, top),       "" + str(IP),  font=font, fill=255)
+    draw.text((x, top),       "" + str(i),  font=font, fill=255)
     #draw.text((x, top+8),     str(CPU), font=font, fill=255)
     #draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
     #draw.text((x, top+25),    str(Disk),  font=font, fill=255)
