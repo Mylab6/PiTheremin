9990839baa42091e87fb785fd17f74146f2abf08
Keith <dreamihad@gmail.com>
2021-03-04 21:36:17 -0500

    ok

---
+++ startScript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# footswitch.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+sudo python3 /home/pi/PiMidi/controller.py
+#cd /
+#cd home/pi/midi_controller
+#sudo python3 midi_controller.py
+#cd /
