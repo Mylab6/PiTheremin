0e5d4d7422a395b338e74167444eecdd4230961d
Keith <dreamihad@gmail.com>
2021-03-06 21:43:22 -0500

    ok

--- SensorRead/TFLuna.py
+++ SensorRead/TFLuna.py
@@ -48,7 +48,9 @@ def read_tfluna_data():
 if ser.isOpen() == False:
     ser.open()  # open serial port if not open
 
-distance, strength, temperature = read_tfluna_data()  # read values
-print('Distance: {0:2.2f} m, Strength: {1:2.0f} / 65535 (16-bit), Chip Temperature: {2:2.1f} C'.
-      format(distance, strength, temperature))  # print sample data
-ser.close()
+while True:
+    distance, strength, temperature = read_tfluna_data()  # read values
+    print('Distance: {0:2.2f} m, Strength: {1:2.0f} / 65535 (16-bit), Chip Temperature: {2:2.1f} C'.
+          format(distance, strength, temperature))  # print sample data
+    ser.close()
+    time.sleep(1)
