abe6706b33f6a89927e10efaa998ced98345bb78
Keith <dreamihad@gmail.com>
2021-03-09 19:00:50 -0500

    code

--- Inputs/dial.py
+++ Inputs/dial.py
@@ -1,75 +1,29 @@
-import os
-import time
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+from RPi import GPIO
+from time import sleep
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+clk = 17
+dt = 18
 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D22)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+counter = 0
+clkLastState = GPIO.input(clk)
 
-# create an analog input channel on pin 0
-chan0 = AnalogIn(mcp, MCP.P0)
+try:
 
-print('Raw ADC Value: ', chan0.value)
-print('ADC Voltage: ' + str(chan0.voltage) + 'V')
+    while True:
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        if clkState != clkLastState:
+            if dtState != clkState:
+                counter += 1
+            else:
+                counter -= 1
 
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 250     # to keep from being jittery we'll only change
-# volume when the pot has moved a significant amount
-# on a 16-bit ADC
-
-
-def remap_range(value, left_min, left_max, right_min, right_max):
-    # this remaps a value from original (left) range to new (right) range
-    # Figure out how 'wide' each range is
-    left_span = left_max - left_min
-    right_span = right_max - right_min
-
-    # Convert the left range into a 0-1 range (int)
-    valueScaled = int(value - left_min) / int(left_span)
-
-    # Convert the 0-1 range into a value in the right range.
-    return int(right_min + (valueScaled * right_span))
-
-
-while True:
-    # we'll assume that the pot didn't move
-    trim_pot_changed = False
-
-    # read the analog pin
-    trim_pot = chan0.value
-    print(trim_pot)
-
-    # how much has it changed since the last read?
-    pot_adjust = abs(trim_pot - last_read)
-
-    if pot_adjust > tolerance:
-        trim_pot_changed = True
-
-    if trim_pot_changed:
-        # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
-        set_volume = remap_range(trim_pot, 0, 65535, 0, 100)
-
-        # set OS volume playback volume
-        print('Volume = {volume}%' .format(volume=set_volume))
-        try:
-            set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' \
-                .format(volume=set_volume)
-            os.system(set_vol_cmd)
-
-        except print(0):
-            pass
-
-        # save the potentiometer reading for the next loop
-        last_read = trim_pot
-
-    # hang out and do nothing for a half second
-    time.sleep(0.5)
+        print(counter)
+        clkLastState = clkState
+        sleep(0.01)
+finally:
+    GPIO.cleanup()
