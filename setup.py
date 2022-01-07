75b3e7bb1a142a5e3b78edfe4fe70f48160ccf8c
Keith <dreamihad@gmail.com>
2022-01-07 18:52:03 -0500

    ok

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-file_object = open('/boot/config.txt, 'a')
+def write_to_config_file():
+    file_object = open('/boot/config.txt', 'a')
 # Append 'hello' at the end of file
-file_object.write("\n")                   
-file_object.write('enable_uart=1')
+    file_object.write("\n")                   
+    file_object.write('enable_uart=1')
 # Close the file
-file_object.close()
-print(' Good to reboot now')
-                  
+    file_object.close()
+    print(' Good to reboot now')
+try:
+    write_to_config_file()
+except: 
+    print('Could not open config.txt, are you root ?')              
