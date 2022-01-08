0b79707cdd757ec09ad4d171c5f92097240cbb04
Keith <dreamihad@gmail.com>
2022-01-07 19:57:36 -0500

    a

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import subprocess
-def write_to_config_file():
-    uart_enable_str = 'enable_uart=1'
-    file_object = open('/boot/config.txt', 'a+')
-    if uart_enable_str in file_object.read():
-        print('No need to update boot config')
+def append_sys_file(text_to_add, file_location):
+    file_object = open(file_location, 'a+')
+    if text_to_add in file_object.read():
+        print('No need to update ', file_location)
     else:
         file_object.write("\n")                   
-        file_object.write(uart_enable_str)
+        file_object.write(text_to_add)
 
     file_object.close()
-    print(' Good to reboot now')
+  
 try:
-    subprocess.run('sudo apt install -y pipenv', shell=True, check=True)
-    write_to_config_file()
+    subprocess.run('pip3 install pipenv', shell=True, check=True)
+    uart_enable_str = 'enable_uart=1'
+    boot_config_file_location = '/boot/config.txt'
+    append_sys_file(uart_enable_str,boot_config_file_location)
+    add_to_path_str = 'export PATH="$HOME/.local/bin:$PATH"'
+    bash_rc_location = '~/.bashrc'
+    append_sys_file(add_to_path_str,bash_rc_location)
+    print(' Good to reboot now')
 except: 
     print('Could not open config.txt, are you root ?')              
