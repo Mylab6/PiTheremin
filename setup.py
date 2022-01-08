23c9eefab822a9d0d9ba3497bce1f5bbd495b55f
Keith <dreamihad@gmail.com>
2022-01-07 20:06:05 -0500

    ok

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 def append_sys_file(text_to_add, file_location):
     print('Wirting to ', file_location)
@@ -16,7 +17,7 @@ try:
     boot_config_file_location = '/boot/config.txt'
     append_sys_file(uart_enable_str,boot_config_file_location)
     add_to_path_str = 'export PATH="$HOME/.local/bin:$PATH"'
-    bash_rc_location = '~/.bashrc'
+    bash_rc_location = os.path.join((os.path.expanduser('~'), '.bashrc'))
     append_sys_file(add_to_path_str,bash_rc_location)
     print(' Good to reboot now')
 except Exception as e: 
