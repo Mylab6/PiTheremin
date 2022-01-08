55bd9eb79fc3c116f426359544d1daa1ab906f59
Keith <dreamihad@gmail.com>
2022-01-07 19:02:45 -0500

    ok

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import subprocess
 def write_to_config_file():
-    file_object = open('/boot/config.txt', 'a')
-# Append 'hello' at the end of file
-    file_object.write("\n")                   
-    file_object.write('enable_uart=1')
-# Close the file
+    uart_enable_str = 'enable_uart=1'
+    file_object = open('/boot/config.txt', 'a+')
+    if uart_enable_str in file_object.read():
+        print('No need to update boot config')
+    else:
+        file_object.write("\n")                   
+        file_object.write(uart_enable_str)
+
     file_object.close()
     print(' Good to reboot now')
 try:
+    subprocess.run('sudo apt install -y pipenv', shell=True, check=True)
     write_to_config_file()
 except: 
     print('Could not open config.txt, are you root ?')              
